363e36425dc35d53a61c081855f23eb2f8955569
Alfonso Dominguez <adominguez@papeleriagamboa.com.mx>
2026-07-06 17:25:32 -0400

    Probar controlador OAuth

--- src/controllers/oauth.controller.js
+++ src/controllers/oauth.controller.js
@@ -1,21 +1,8 @@
-const {
-    clientId,
-    redirectUri,
-} = require("../config/tiendanube.config");
-
 function instalar(req, res) {
-    if (!clientId || !redirectUri) {
-        return res.status(500).json({
-            success: false,
-            message: "Faltan variables de entorno de Tienda Nube.",
-        });
-    }
-
-    const url =
-        `https://www.tiendanube.com/apps/${clientId}/authorize` +
-        `?redirect_uri=${encodeURIComponent(redirectUri)}`;
-
-    return res.redirect(url);
+    return res.status(418).json({
+        success: true,
+        message: "ESTE ES EL NUEVO CONTROLADOR",
+    });
 }
 
 module.exports = {
